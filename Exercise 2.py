cc634a71c821720598bbd4e3d3ee280f158f1ef6
Gralhix <74664918+Gralhix@users.noreply.github.com>
2021-06-29 13:33:59 +0100

    Add files via upload

---
+++ Exercise 2.py
@@ -0,0 +1,10 @@
+# https://www.udemy.com/course/solving-exercises-with-python
+# Exercise 2: Given a list of numbers, return True is first and last number of a list is same.
+
+def ex2(n1):
+    return n1[0] == n1[-1]
+
+print(ex2([1, 22, 4, 565, 1]))
+    
+    
+# second list to try: [4, 62, 421, 56, 12]
