57a0def2cc59557075c93649a2ed9c2364004a45
Gralhix <74664918+Gralhix@users.noreply.github.com>
2021-06-29 13:46:49 +0100

    Add files via upload

---
+++ Exercise 3.py
@@ -0,0 +1,10 @@
+# https://www.udemy.com/course/solving-exercises-with-python
+# Exercise 3: Given a list of numbers, iterate it and print only those numbers which are divisible by 5.
+
+
+n1 = [2, 4, 88, 43, 535, 0, 10, 45]
+
+for i in n1:
+    if i % 5 == 0:
+        print(i)
+        
