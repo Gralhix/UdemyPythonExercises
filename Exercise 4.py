389f84ec5b69a585969b6e9ae3415c1f592f2dbb
Gralhix <74664918+Gralhix@users.noreply.github.com>
2021-06-29 13:53:27 +0100

    Add files via upload

---
+++ Exercise 4.py
@@ -0,0 +1,9 @@
+# https://www.udemy.com/course/solving-exercises-with-python
+# Exercise 4: Return the count of sub-string "John" appears in the given string "John is good developer. John is a writer".
+
+sentence = "John is good developer. John is a writer"
+word = "John"
+
+n = sentence.count(word)
+
+print(n)
