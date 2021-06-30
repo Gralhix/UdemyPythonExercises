002522dda3ed341e2c46ddd191c8ca973ed97ae0
Gralhix <74664918+Gralhix@users.noreply.github.com>
2021-06-30 15:44:44 +0100

    Add files via upload

---
+++ Exercise 8.py
@@ -0,0 +1,10 @@
+# https://www.udemy.com/course/solving-exercises-with-python
+# Exercise 8: Print the following pattern:
+# *
+# **
+# ***
+# ****
+# *****
+
+for i in range(0,6):
+    print("*" * i)
